21439b957e3f32c8cc74f0d9bb10c53a1bd7811c
CristianAd <sonlatin2@gmail.com>
2022-12-22 15:10:44 -0500

    Segundo Ejercicio OpenBootcam

---
+++ Ej2/Ej2.js
@@ -0,0 +1,18 @@
+const miNombre = "Cristian";
+
+const miEdad = 29;
+
+let desarrollador = false;
+
+const fechaNacimiento = new Date("july 01 1993");
+
+
+const libroFavorito = {
+    titulo: "De cero a uno",
+    autor: "Peter Thiel ",
+    fecha: "2014",
+    url: "https://www.perlego.com/book/2539222/de-cero-a-uno-cmo-inventar-el-futuro-pdf"
+ }
+
+
+ console.log(libroFavorito)
