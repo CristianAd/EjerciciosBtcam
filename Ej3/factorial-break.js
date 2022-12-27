6f6c19ac4ea2587a0abd3389a5172bd0b64efd81
CristianAd <sonlatin2@gmail.com>
2022-12-26 19:22:08 -0500

    Tercer ejercicio OpenBootcam

---
+++ Ej3/factorial-break.js
@@ -0,0 +1,15 @@
+
+let factorialDe = 10
+let respuesta = 1
+
+let i = factorialDe
+while (true) {
+    if (i <= 1) {
+      break;
+    }
+    respuesta = respuesta * i;
+    i--;
+    console.log(respuesta)
+  }
+
+ 
---
+++ Ej3/factorial-for.js
@@ -0,0 +1,33 @@
+
+
+//factorial de un número
+
+let factorialDe = 10
+let respuesta = 1
+
+// Ejercicio 1
+//1*10 =10
+//10*9 = 90
+//90*8  = 720
+//720*7 = 5040
+
+
+for (let i = factorialDe; i >= 1; i--) {
+
+    respuesta = respuesta * i
+   
+    console.log(respuesta)
+  }
+
+//Ejercicio 2
+let factDe = 10
+let resp = 1
+
+let j = factDe
+while ( j >= 1) {
+    resp = resp * j
+    console.log(resp)
+    j--;
+}
+
+//Ejercicio 3
