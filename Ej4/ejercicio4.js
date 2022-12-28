ad8e5f709aa0b666f0fe2a2d1befaf41e381828f
CristianAd <sonlatin2@gmail.com>
2022-12-28 13:39:34 -0500

    Ejercicio 4

---
+++ Ej4/ejercicio4.js
@@ -0,0 +1,23 @@
+
+//Ej1
+let nombre = "Cristian";
+//Ej2
+let apellido = "Arias";
+//Ej3
+let estudiante = nombre + " "+ apellido
+//Ej4
+let estudianteMayus = estudiante.toUpperCase()
+//Ej5
+let estudianteMinus = estudiante.toLowerCase()
+//Ej6
+let numLetrasEstudiante = estudiante.length
+//Ej7
+let primerLetraNombre =  nombre.charAt(0)
+//Ej8
+let ultimaLetraApellido =  apellido.charAt(4)
+//Ej9
+let textoSinEspaciosEstudiante = estudiante.replace(/\s/g, "") 
+ //Ej10
+let variableContieneEstudiante = estudiante.includes("Cristian")
+console.log(variableContieneEstudiante)
+ 
