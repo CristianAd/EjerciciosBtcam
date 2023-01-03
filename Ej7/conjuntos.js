b9c38d3429121550a978d0f157683b1e86f35154
CristianAd <sonlatin2@gmail.com>
2023-01-03 11:54:57 -0500

    ejercicio siete

---
+++ Ej7/conjuntos.js
@@ -0,0 +1,24 @@
+/*
+Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+- Un nuevo Set con los nombres de tu familia
+
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+*/
+
+
+let nfamilia = ["Robert", "Camilo", "Cristian", "Rosa", "Elena", "Lupe"]
+
+const xfamilia = new Set(nfamilia)
+console.log(xfamilia)
+
+
+const agregoNuevo = xfamilia.add("Cristian")
+console.log(agregoNuevo)
+
+
+const agregoOtro = xfamilia.add("Javascript")
+console.log(agregoOtro)
+
