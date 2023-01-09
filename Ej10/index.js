8bc1a07eda556f990cf8c310e31dd3a85203ab61
Cristian <sonlatin2@gmail.com>
2023-01-09 18:32:58 -0500

    ejercicio diez

---
+++ Ej10/index.js
@@ -0,0 +1,16 @@
+
+import { suma } from "./controller.js"
+import chalk from 'chalk';
+
+
+console.log(suma(1,2))
+
+console.log(chalk.green(suma(4,5)))
+
+ 
+
+
+
+
+//  console.log(moduloController.suma(4,5))
+ 
