a54d5808204de80f9ef18df504dbee0ab32f3b8f
Cristian <sonlatin2@gmail.com>
2023-01-11 15:39:32 -0500

    ejercicio 13 eslint

---
+++ Ej13/ejercicio4.js
@@ -0,0 +1,23 @@
+
+//Ej1
+let nombre =  "Crisitan"
+//Ej2
+let apellido = "Arias"
+//Ej3
+let estudiante = `${nombre} + ${apellido}`
+//Ej4
+let estudianteMayus = estudiante.toUpperCase()
+//Ej5
+let estudianteMinus = estudiante.toLowerCase()
+//Ej6
+let numLetrasEstudiante = estudiante.length
+//Ej7
+let primerLetraNombre =  nombre.charAt(0)
+//Ej8
+let ultimaLetraApellido =  apellido.charAt(4)
+//Ej9
+let textoSinEspaciosEstudiante = estudiante.replace(/\s/g, "") 
+//Ej1
+let variableContieneEstudiante = estudiante.includes("Cristian")
+console.log(variableContieneEstudiante)
+ 
