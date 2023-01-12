5125402d2bf725bdb348a3a378edb60d556c13fb
Cristian <sonlatin2@gmail.com>
2023-01-12 13:22:19 -0500

    ejercicio catorce sobre jQuery

---
+++ Ej14/index.js
@@ -0,0 +1,19 @@
+
+
+const elBtn = document.getElementById("btn")
+elBtn.addEventListener("click",  ()=>{
+    alert("click en el botón")  
+})
+
+
+$( ()=>{
+
+    $("#btn-2").click( ()=>{
+         
+        console.log("Hola, estoy utilizando jQuery")
+
+    })
+
+})
+
+ 
